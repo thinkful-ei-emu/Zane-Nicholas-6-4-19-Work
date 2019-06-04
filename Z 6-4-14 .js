350dba51523440f0b370cec13668542e2d77f4f4
Nicholas Gunter <ygnick@live.com>
2019-06-04 12:30:41 -0400

    added repeat function

--- Z 6-4-14 .js
+++ Z 6-4-14 .js
@@ -39,8 +39,27 @@ function findAverage(num){
   }); return total / num.length;
 }
 
+// Higher order function drills
 
-  
+function repeat(fn, n){
+  for (let i = 0; i < n; i++){
+    fn();
+  }
+}
+
+function hello(){
+  console.log('hello world');
+}
+
+function goodbye(){
+  console.log('goodbye world');
+}
+
+repeat(hello, 5);
+
+repeat(goodbye, 5);
+
+
 
 
 
